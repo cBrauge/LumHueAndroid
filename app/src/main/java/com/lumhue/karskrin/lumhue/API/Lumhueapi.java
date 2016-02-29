14372206754cfeff1b7b9e2c6010591fee597444
Karskrin <braugecorentin@gmail.com>
2016-02-29 01:49:01 +0100

    android: post method and select light to enable

--- app/src/main/java/com/lumhue/karskrin/lumhue/API/Lumhueapi.java
+++ app/src/main/java/com/lumhue/karskrin/lumhue/API/Lumhueapi.java
@@ -5,20 +5,19 @@ import com.lumhue.karskrin.lumhue.model.Lumhuemodel;
 import java.util.List;
 
 import retrofit.Callback;
-import retrofit.http.Body;
 import retrofit.http.GET;
+import retrofit.http.Multipart;
 import retrofit.http.POST;
+import retrofit.http.Part;
 
-/**
- * Created by Karskrin on 27/02/2016.
- */
 public interface Lumhueapi {
     @GET("/lights")
         //here is the other url part.best way is to start using /
     void getLights(Callback<List<Lumhuemodel>> response);
     //response is the response from the server which is now in the POJO
+    @Multipart
     @POST("/lights")
     //here is the other url part.best way is to start using /
-    void postLights(@Body Integer id, @Body status, Callback<List<Lumhuemodel>> response);
+    void postLights(@Part("id") Integer id, @Part("status") Integer status, Callback<List<Lumhuemodel>> response);
     //response is the response from the server which is now in the POJO
 }
--- app/src/main/java/com/lumhue/karskrin/lumhue/View/Lights.java
+++ app/src/main/java/com/lumhue/karskrin/lumhue/View/Lights.java
@@ -5,38 +5,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.lumhue.karskrin.lumhue.API.Lumhueapi;
 import com.lumhue.karskrin.lumhue.R;
-import com.lumhue.karskrin.lumhue.model.Light;
 import com.lumhue.karskrin.lumhue.model.Lumhuemodel;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-/**
- * Created by Karskrin on 27/02/2016.
- */
 public class Lights extends Fragment {
     String API = "https://lumhue.mr-calen.eu";
     Button click;
     Button post;
     ProgressBar pbar;
+    private Switch swi;
     private LinearLayout mLayout;
     private ListView mListView;
     private ArrayList<String> adapter;
     private ArrayAdapter<String> adapterr;
+    private boolean check = true;
+    private Integer itemIndex = 0;
+    private TextView itemSelected;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,9 +63,26 @@ public class Lights extends Fragment {
         click = (Button) getView().findViewById(R.id.button);
         post = (Button) getView().findViewById(R.id.buttonPost);
         pbar = (ProgressBar) getView().findViewById(R.id.pb);
+        itemSelected = (TextView) getView().findViewById(R.id.itemSelected);
         mListView = (ListView) getView().findViewById(R.id.listView);
-        adapter = new ArrayList<String>();
-        adapterr = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                itemIndex = position;
+                itemSelected.setText(itemIndex.toString());
+                post.setEnabled(true);
+            }
+        });
+        swi = (Switch) getView().findViewById(R.id.switch1);
+        swi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                check = isChecked;
+            }
+        });
+
+        adapter = new ArrayList<>();
+        adapterr = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, adapter);
         mListView.setAdapter(adapterr);
         pbar.setVisibility(View.INVISIBLE);
         get();
@@ -72,13 +92,12 @@ public class Lights extends Fragment {
                 get();
             }
         });
-        post.setOnClickListener(new View.OnClickListener()){
+        post.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 post();
             }
-        }
+        });
     }
 
     private void get() {
@@ -117,12 +136,10 @@ public class Lights extends Fragment {
         pbar.setVisibility(View.VISIBLE);
         adapterr.clear();
         adapterr.notifyDataSetChanged();
-        lumhueapi.postLights(1, 1, new Callback<List<Lumhuemodel>>() {
+        lumhueapi.postLights(itemIndex, check ? 1 : 0, new Callback<List<Lumhuemodel>>() {
             @Override
-            public void success(List<Lumhuemodel> lumhuemodel, Response response) {
-                // go on "/" String tv = "Bridge is online :" + lumhuemodel.getBridgeIsOnline();
-                //adapter.add(tv);
-                for (Lumhuemodel entry : lumhuemodel) {
+            public void success(List<Lumhuemodel> lumhuemodels, Response response) {
+                for (Lumhuemodel entry : lumhuemodels) {
                     String light = entry.getId() + " is "
                             + (entry.getOn() == 1 ? "on" : "off");
                     adapter.add(light);
