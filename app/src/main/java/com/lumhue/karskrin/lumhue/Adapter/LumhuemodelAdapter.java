6c5b0340d13eadf31dc977c490fa71e3574fd4ec
Karskrin <braugecorentin@gmail.com>
2016-02-29 11:22:58 +0100

    android: custom adapter for lights and single post button

---
+++ app/src/main/java/com/lumhue/karskrin/lumhue/Adapter/LumhuemodelAdapter.java
@@ -0,0 +1,109 @@
+package com.lumhue.karskrin.lumhue.Adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.lumhue.karskrin.lumhue.API.Lumhueapi;
+import com.lumhue.karskrin.lumhue.R;
+import com.lumhue.karskrin.lumhue.View.Lights;
+import com.lumhue.karskrin.lumhue.model.Lumhuemodel;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class LumhuemodelAdapter extends ArrayAdapter<Lumhuemodel> {
+    List<Lumhuemodel> lights = null;
+    Lights context;
+    int layoutResourceId;
+    String API = "https://lumhue.mr-calen.eu";
+
+    public LumhuemodelAdapter(Lights context, int resource, List<Lumhuemodel> objects) {
+        super(context.getActivity().getBaseContext(), resource, objects);
+        this.layoutResourceId = resource;
+        this.context = context;
+        this.lights = objects;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        LumhuemodelHolder holder;
+        if (row == null) {
+            LayoutInflater inflater = (context.getActivity()).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new LumhuemodelHolder();
+            holder.swi = (Switch) row.findViewById(R.id.swiRow);
+            holder.txtview = (TextView) row.findViewById(R.id.txtRow);
+            holder.swi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    lights.get(position).setOn(isChecked ? 1 : 0);
+                }
+            });
+            holder.send = (Button) row.findViewById(R.id.send);
+            holder.progressBar = (ProgressBar) row.findViewById(R.id.progressBar);
+            holder.progressBar.setVisibility(View.INVISIBLE);
+            holder.model = lights.get(position);
+            final LumhuemodelHolder finalHolder = holder;
+            holder.send.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    post(finalHolder);
+                }
+            });
+            row.setTag(holder);
+        } else {
+            holder = (LumhuemodelHolder) row.getTag();
+        }
+
+        Lumhuemodel model = lights.get(position);
+        holder.txtview.setText(model.getId() + " is "
+                + (model.getOn() == 1 ? "on" : "off"));
+        holder.swi.setChecked(model.getOn() == 1);
+
+        return row;
+    }
+
+    private void post(final LumhuemodelHolder holder) {
+        RestAdapter restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).setEndpoint(API).build();
+        final Lumhueapi lumhueapi = restAdapter.create(Lumhueapi.class);
+        holder.progressBar.setVisibility(View.VISIBLE);
+        lumhueapi.postLights(Integer.valueOf(holder.model.getId()), holder.model.getOn(), new Callback<List<Lumhuemodel>>() {
+            @Override
+            public void success(List<Lumhuemodel> lumhuemodels, Response response) {
+                holder.progressBar.setVisibility(View.INVISIBLE);
+                context.get();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                String tv = error.getMessage();
+                Log.v("LIGHTS fragment", tv);
+                holder.progressBar.setVisibility(View.INVISIBLE);
+                context.get();
+            }
+        });
+
+    }
+
+    static class LumhuemodelHolder {
+        TextView txtview;
+        Switch swi;
+        Button send;
+        ProgressBar progressBar;
+        Lumhuemodel model;
+    }
+}
--- app/src/main/java/com/lumhue/karskrin/lumhue/View/Lights.java
+++ app/src/main/java/com/lumhue/karskrin/lumhue/View/Lights.java
@@ -2,20 +2,17 @@ package com.lumhue.karskrin.lumhue.View;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Switch;
-import android.widget.TextView;
 
 import com.lumhue.karskrin.lumhue.API.Lumhueapi;
+import com.lumhue.karskrin.lumhue.Adapter.LumhuemodelAdapter;
 import com.lumhue.karskrin.lumhue.R;
 import com.lumhue.karskrin.lumhue.model.Lumhuemodel;
 
@@ -30,22 +27,16 @@ import retrofit.client.Response;
 public class Lights extends Fragment {
     String API = "https://lumhue.mr-calen.eu";
     Button click;
-    Button post;
     ProgressBar pbar;
-    private Switch swi;
     private LinearLayout mLayout;
     private ListView mListView;
-    private ArrayList<String> adapter;
-    private ArrayAdapter<String> adapterr;
-    private boolean check = true;
-    private Integer itemIndex = 0;
-    private TextView itemSelected;
+    private ArrayList<Lumhuemodel> adapter;
+    private LumhuemodelAdapter adapterr;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-
     }
 
     @Override
@@ -61,28 +52,11 @@ public class Lights extends Fragment {
         // Inflate the layout for this fragment
         mLayout = (LinearLayout) getView().findViewById(R.id.lightsLayout);
         click = (Button) getView().findViewById(R.id.button);
-        post = (Button) getView().findViewById(R.id.buttonPost);
         pbar = (ProgressBar) getView().findViewById(R.id.pb);
-        itemSelected = (TextView) getView().findViewById(R.id.itemSelected);
         mListView = (ListView) getView().findViewById(R.id.listView);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                itemIndex = position;
-                itemSelected.setText(itemIndex.toString());
-                post.setEnabled(true);
-            }
-        });
-        swi = (Switch) getView().findViewById(R.id.switch1);
-        swi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                check = isChecked;
-            }
-        });
 
         adapter = new ArrayList<>();
-        adapterr = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, adapter);
+        adapterr = new LumhuemodelAdapter(this, R.layout.listview_light_row, adapter);
         mListView.setAdapter(adapterr);
         pbar.setVisibility(View.INVISIBLE);
         get();
@@ -92,29 +66,18 @@ public class Lights extends Fragment {
                 get();
             }
         });
-        post.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                post();
-            }
-        });
     }
 
-    private void get() {
+    public void get() {
         RestAdapter restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).setEndpoint(API).build();
         final Lumhueapi lumhueapi = restAdapter.create(Lumhueapi.class);
         pbar.setVisibility(View.VISIBLE);
         adapterr.clear();
-        adapterr.notifyDataSetChanged();
         lumhueapi.getLights(new Callback<List<Lumhuemodel>>() {
             @Override
             public void success(List<Lumhuemodel> lumhuemodel, Response response) {
-                // go on "/" String tv = "Bridge is online :" + lumhuemodel.getBridgeIsOnline();
-                //adapter.add(tv);
                 for (Lumhuemodel entry : lumhuemodel) {
-                    String light = entry.getId() + " is "
-                            + (entry.getOn() == 1 ? "on" : "off");
-                    adapter.add(light);
+                    adapter.add(entry);
                     adapterr.notifyDataSetChanged();
                 }
                 pbar.setVisibility(View.INVISIBLE);
@@ -123,36 +86,7 @@ public class Lights extends Fragment {
             @Override
             public void failure(RetrofitError error) {
                 String tv = error.getMessage();
-                adapter.add(tv);
-                adapterr.notifyDataSetChanged();
-                pbar.setVisibility(View.INVISIBLE);
-            }
-        });
-    }
-    private void post()
-    {
-        RestAdapter restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).setEndpoint(API).build();
-        final Lumhueapi lumhueapi = restAdapter.create(Lumhueapi.class);
-        pbar.setVisibility(View.VISIBLE);
-        adapterr.clear();
-        adapterr.notifyDataSetChanged();
-        lumhueapi.postLights(itemIndex, check ? 1 : 0, new Callback<List<Lumhuemodel>>() {
-            @Override
-            public void success(List<Lumhuemodel> lumhuemodels, Response response) {
-                for (Lumhuemodel entry : lumhuemodels) {
-                    String light = entry.getId() + " is "
-                            + (entry.getOn() == 1 ? "on" : "off");
-                    adapter.add(light);
-                    adapterr.notifyDataSetChanged();
-                }
-                pbar.setVisibility(View.INVISIBLE);
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                String tv = error.getMessage();
-                adapter.add(tv);
-                adapterr.notifyDataSetChanged();
+                Log.v("LIGHTS fragment", tv);
                 pbar.setVisibility(View.INVISIBLE);
             }
         });
